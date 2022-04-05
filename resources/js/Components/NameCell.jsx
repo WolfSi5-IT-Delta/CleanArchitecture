62d2434fa9a085b8571bc312901a74f2a4e8dd37
Vasiliy Sizov <prog8@it-delta.ru>
2022-04-05 09:35:03 +0300

    hotfix

--- resources/js/Components/NameCell.jsx
+++ resources/js/Components/NameCell.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const NameCell = ({ value }) => {
   return (
-    <div className="flex">
+    <div className="flex items-center h-full">
       <div className="flex-shrink-0 h-7 w-7">
         <img className="h-7 w-7 rounded-full object-cover" src={value.image} alt="" />
       </div>
       <div className="flex-shrink-1 pl-4 w-full">
-        <div className="truncate">{`${value.name} ${value.last_name}`}</div>
+        <div className="truncate">{`${value.name} ${value.last_name !== undefined ? value.last_name : ''}`}</div>
       </div>
     </div>
   );
