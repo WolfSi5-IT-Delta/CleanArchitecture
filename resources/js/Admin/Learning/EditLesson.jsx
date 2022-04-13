f2fbd4dfa6270481e51ee20f0a8c169555845382
makasinui <mks.shi@ya.ru>
2022-04-13 16:28:03 +0300

    fix saving lesson editing

--- resources/js/Admin/Learning/EditLesson.jsx
+++ resources/js/Admin/Learning/EditLesson.jsx
@@ -157,6 +157,7 @@ export default function EditLesson({ lesson, all_questions }) {
               <textarea
                 className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 border-gray-300 rounded-md"
                 defaultValue={data.description}
+                onChange={(e)=>setData('description',e.target.value)}
               />
             </li>
             <li className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -164,6 +165,7 @@ export default function EditLesson({ lesson, all_questions }) {
               <textarea
                 className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 border-gray-300 rounded-md"
                 defaultValue={data.detail_text}
+                onChange={(e)=>setData('detail_text',e.target.value)}
               />
             </li>
             <li className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
