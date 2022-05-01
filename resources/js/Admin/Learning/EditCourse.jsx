8cb0c87af63f073893b0c7f5575b679090f1a1f1
Maxim Gerzon <gerzon@it-delta.ru>
2022-05-01 22:17:09 +0300

    fix for buttons on the EditCourse page

--- resources/js/Admin/Learning/EditCourse.jsx
+++ resources/js/Admin/Learning/EditCourse.jsx
@@ -288,16 +288,17 @@ export default function EditCourse({ course, all_lessons, permissions, allPermis
                 {data.permissions.map(item => {
                   return(
                     <li key={`sperm${item.type}_${item.id}`}
-                        className='inline-flex items-center py-0.5 px-1 m-1 rounded-full text-xs font-medium bg-gray-100 text-gray-700'
+                        className="inline-flex items-center py-0.5 pl-2 pr-0.5 m-1 rounded-full text-xs font-medium bg-gray-100 text-gray-700
+                           cursor-pointer hover:bg-gray-200"
+                        onClick={() => {
+                          removePermission(item);
+                        }}
                     >
                       {item.name}
-
                     <button
                       type="button"
                       className="flex-shrink-0 ml-0.5 h-4 w-4 rounded-full inline-flex items-center justify-center text-gray-400 hover:bg-gray-200 hover:text-gray-500 focus:outline-none focus:bg-gray-500 focus:text-white"
-                      onClick={() => {
-                        removePermission(item);
-                      }}>
+                    >
                       <svg className="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
                         <path strokeLinecap="round" strokeWidth="1.5" d="M1 1l6 6m0-6L1 7"/>
                       </svg>
