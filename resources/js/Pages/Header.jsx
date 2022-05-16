84587957a52da38699a23d4f79031e4595c2ac73
itdelta-prog <delta@it-delta.ru>
2022-05-16 09:19:49 +0300

    profile menu fix

--- resources/js/Pages/Header.jsx
+++ resources/js/Pages/Header.jsx
@@ -107,7 +107,8 @@ export default function Header({ children }) {
                   </div>
                   <div className="-mr-2 flex items-center sm:hidden">
                     {/* Mobile menu button */}
-                    <Disclosure.Button
+                    <Menu as="div" className="ml-3 relative">
+                    {/* <Menu.Button
                       className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                       <span className="sr-only">Open main menu</span>
                       {open ? (
@@ -115,12 +116,63 @@ export default function Header({ children }) {
                       ) : (
                         <MenuIcon className="block h-6 w-6" aria-hidden="true"/>
                       )}
-                    </Disclosure.Button>
+                    </Menu.Button> */}
+                      <div>
+                        <Menu.Button
+                          className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                          <span className="sr-only">Open user menu</span>
+                          <img className="h-8 w-8 rounded-full" src={user?.avatar || '/img/no-user-photo.jpg'} alt=""/>
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items
+                          className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
+                          {mainMenu.map((item) => (
+                              <InertiaLink
+                                key={item.name}
+                                href={item.href}
+                                className={classNames(
+                                  item.current
+                                    ? 'bg-gray-100' : '',
+                                    'block px-4 py-2 text-sm text-gray-700'
+                                )}
+                                aria-current={item.current ? 'page' : undefined}
+                              >
+                                {item.name}
+                              </InertiaLink>
+                            ))}
+                          {userMenu.map((item) => (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => (
+                                <InertiaLink
+                                  href={item.href}
+                                  className={classNames(
+                                    active ? 'bg-gray-100' : '',
+                                    'block px-4 py-2 text-sm text-gray-700'
+                                  )}
+                                >
+                                  {item.name}
+                                </InertiaLink>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
                   </div>
                 </div>
               </div>
+              
 
-              <Disclosure.Panel className="sm:hidden">
+              {/* <Disclosure.Panel className="sm:hidden">
                 <div className="pt-2 pb-3 space-y-1">
                   {mainMenu.map((item) => (
                     <InertiaLink
@@ -138,6 +190,8 @@ export default function Header({ children }) {
                     </InertiaLink>
                   ))}
                 </div>
+
+                
                 <div className="pt-4 pb-3 border-t border-gray-200">
                   <div className="flex items-center px-4">
                     <div className="flex-shrink-0">
@@ -167,7 +221,7 @@ export default function Header({ children }) {
                     ))}
                   </div>
                 </div>
-              </Disclosure.Panel>
+              </Disclosure.Panel> */}
             </>
           )}
         </Disclosure>
