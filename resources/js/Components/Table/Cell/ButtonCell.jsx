99ee6fb4715832ce7e1a0da208ca92d5f419c47e
itdelta-prog <delta@it-delta.ru>
2022-05-31 11:17:54 +0300

    date formatting

--- resources/js/Components/Table/Cell/ButtonCell.jsx
+++ resources/js/Components/Table/Cell/ButtonCell.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 const ButtonCell = ({ value: {onClick} }) => {
   return (
-    <button
+    <div className="w-full grid justify-items-center">
+      <button button
         type="button"
         className="inline-flex justify-center w-50 rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white
             focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm
             bg-indigo-500 hover:bg-indigo-700"
         onClick={onClick}
-    >Check
-  </button>
+      >Check
+      </button>
+    </div>
   );
 };
 
