c87e1a03a846706eba17d87ebb600f60943b8226
Maxim Gerzon <gerzon@it-delta.ru>
2022-07-17 20:46:55 +0300

    added typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,7 @@ module.exports = {
 
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio')
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography')
   ],
 };
